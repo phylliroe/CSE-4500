e40124566da3249dbffd2f5a66f0b8a3b173fd66
andy <andy@localhost.localdomain>
2021-01-26 15:06:04 -0800

    nodejs test

---
+++ test.js
@@ -0,0 +1,8 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.end('Hello World');_
+}).listen(5000);
+
+console.log("Running on 127.0.0.1:5000/");
