009741e8344f5f8f4b4ee91a730496d1698b0f1e
andy <andy@localhost.localdomain>
2021-01-31 12:01:24 -0800

    more socket testing

--- express_test.js
+++ express_test.js
@@ -13,34 +13,15 @@ io.on('connection', socket => {
     socket.on('disconnect', () => {
         console.log("client is gone");
     });
+
+    // emit to all clients
+    socket.on('message', data => {
+        console.log(data);
+        io.emit("message_sent", data);
+    });
 });
+
 
 http.listen(port, () => {
     console.log(`server started on ${port}`);
 });
-
-/*
-const express = require('express');
-const http = require('http');
-const app = express();
-//const http = require('http').Server(app);
-const server = http.createServer(app)
-const port = 8080;
-
-var socketio = require('socket.io')(server);
-
-app.get('/', (req, res) => {
-    //res.send('Hello World!');
-    res.sendFile(__dirname + "/socket.html");
-});
-
-socketio.on('connection', (client) => {
-    console.log("Client connected!");
-});
-
-/*
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Running on ${port}`);
-});*/
-
-//server.listen(port);
--- public/main.js
+++ public/main.js
@@ -4,3 +4,14 @@ var socket = io("http://localhost:8080");
 socket.on("hello", data => {
     console.log("server said hey");
 });
+
+socket.on("message_sent", msg => {
+    let new_msg = `<li>${msg}</li>`;
+    document.getElementById("messages").innerHTML += new_msg;
+});
+
+function send() {
+    let txt = document.getElementById("text").value;
+    console.log(txt);
+    socket.emit("message", txt);
+}
