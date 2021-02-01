2d1cac199abe5961face090001426ffba5719b1a
andy <andy@localhost.localdomain>
2021-02-01 12:36:34 -0800

    still messing with socketio

--- express_test.js
+++ express_test.js
@@ -4,11 +4,13 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
 
+var clients = []
+
 app.use(express.static(`${__dirname}/public`));
 
 io.on('connection', socket => {
     console.log("connected")
-    socket.emit("hello", {data: "data"});
+    socket.emit("hello", {data: socket.username});
 
     socket.on('disconnect', () => {
         console.log("client is gone");
@@ -19,8 +21,16 @@ io.on('connection', socket => {
         console.log(data);
         io.emit("message_sent", data);
     });
-});
 
+    socket.on('name', data => {
+        console.log(`${data} connected!`);
+        socket.username = data;
+        console.log("username is: " + socket.username);
+        clients.push(data);
+        console.log(clients);
+        console.log("There is now " + clients.length + " clients");
+    });
+});
 
 http.listen(port, () => {
     console.log(`server started on ${port}`);
--- public/main.js
+++ public/main.js
@@ -1,8 +1,13 @@
 console.log("script is working");
 var socket = io("http://localhost:8080");
 
+window.onload = () => {
+    enter_name();
+}
+
 socket.on("hello", data => {
     console.log("server said hey");
+    console.log(data);
 });
 
 socket.on("message_sent", msg => {
@@ -14,4 +19,10 @@ function send() {
     let txt = document.getElementById("text").value;
     console.log(txt);
     socket.emit("message", txt);
+}
+
+function enter_name() {
+    let name = prompt("Enter name: ");
+
+    socket.emit('name', name);
 }
